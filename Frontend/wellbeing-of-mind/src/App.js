3c33cfdcf970ccfbd91cc4afa3b53300de6753ab
Sofiiia18 <sonasavcenko8@gmail.com>
2023-11-12 21:39:39 +0100

    feat:create some basic quiz test logic with style

--- Frontend/wellbeing-of-mind/src/App.js
+++ Frontend/wellbeing-of-mind/src/App.js
@@ -2,19 +2,29 @@ import React from "react";
 import Layout from "./pages/Layout";
 import Articles from "./pages/Articles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tests from './pages/Tests/Tests'
+import { jsQuizz } from './pages/Tests/constatants';
 
 function App() {
   return (
+    
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/articles" element={<Articles />}></Route>
+            <Route path="/tests" element={<Tests questions={jsQuizz.questions} />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
+   
+     
   );
+    {/* <Tests questions={jsQuizz.questions} />; */}
+
+   
+ 
 }
 
 export default App;
